90a5e2a0f6b34e84b8dd7ded528fd5ff068c85a3
reneoun <43538982+reneoun@users.noreply.github.com>
2025-09-21 23:28:06 +0200

    begin with md

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import { marked } from "marked";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const textArea = document.querySelector("textarea#editor") as HTMLTextAreaElement;
+    const preview = document.querySelector("div#preview") as HTMLDivElement;
+    const sectionA4 = document.querySelector("section[a4]") as HTMLElement;
+
+    if (sectionA4) {
+        const onClick = () => {
+            if (textArea) textArea.focus();
+        };
+        sectionA4.addEventListener("click", onClick);
+        sectionA4.addEventListener("touchstart", onClick);
+    }
+
+    textArea.addEventListener("input", () => {
+        const markdown = textArea.value;
+        let html = marked(markdown);
+        if (typeof html !== "string") {
+            html = String(html);
+        }
+        if (preview) preview.innerHTML = html;
+    });
+});
